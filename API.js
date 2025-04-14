7c5f42ef89c24d85be309de049f8ce4ccd1247b0
Pedro <lofsbatman@gmail.com>
2025-04-14 18:06:11 -0300

    [Heroku] Preparando GIT

---
+++ API.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const axios = require('axios');
+const env = require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/',(req, res) => {
+    res.send('Funcionado');
+});
+
+app.get('/products', async (req, res) => {
+    try{
+        const resposta = await axios.get('https://fakestoreapi.com');
+        res.json(resposta.data);
+    }catch(error){
+        console.log("erro ao Buscar produtos: "+ error.mensagem + error.status());
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+  });
